4703aece389148bfee339b1ce2e192bc4ed30d94
timurgordon <timurgordon@gmail.com>
2022-07-25 13:47:46 +0200

    initial commit

---
+++ content/header/_index.md
@@ -0,0 +1,15 @@
+---
+title: "Header"
+description: "NEAR is on a mission to empower everyone to take back control of their money, their data, and their identity. Join us."
+insert_anchor_links: "left"
+template: "partials/header.html"
+extra:
+  logoPath: "/images/OW_logo.png"
+  imgPath: ""
+---
+
+- [Getting Started]("/getting-started")
+- [Shortcodes]("shortcodes")
+- [Partials]("/partials")
+- [Templates]("/blog")
+- [Resources]("/resources")
---
+++ tailwind.config.js
@@ -0,0 +1,64 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+      './templates/**/*.html'
+  ],
+  darkMode: false,
+  variants: {
+      extend: {
+          first: ['active'],
+          last: ['active']
+        }
+  },
+  theme: {
+    fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'md': '1.05rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+        '8xl': '5.75rem',
+        '9xl':	'8rem' 
+    },
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      black: '#000000',
+      gray: colors.gray,
+      slate: colors.slate,
+      stone: colors.stone,
+      zinc: colors.zinc,
+      yellow: colors.yellow,
+      orange: colors.orange,
+      sky: colors.sky,
+      blue: {
+        '100': '#bbeeff',
+        '200': '#00aaff',
+        '300': '#0088ff',
+        '400': '#0033ff',
+        '500': '#0000ff',
+        '600': '#0000bb',
+        '700': '#0000aa',
+        '800': '#000088',
+        '900': '#000066',
+      },
+      
+    },
+    extend: {
+        screens: {
+          '2xl': '1450px',
+          // => @media (min-width: 992px) { ... }
+        },
+      },
+  },
+  plugins: [], // if we add forms, do it here
+}
